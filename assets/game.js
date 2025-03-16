0bc9c16b9ad9345135012bf789bb4d5029bed00b
leosupra <leosupra@protonmail.com>
2025-03-16 00:54:05 +0000

    Update game.js

--- assets/game.js
+++ assets/game.js
@@ -20,7 +20,7 @@ function setup() {
   canvas = createCanvas(windowWidth, windowHeight);
   centerCanvas();
   background(0, 0, 0);
-  textFont('Tahoma');
+  textFont('Arial Black');
   textSize(24);
   
   // Initial lion position
