22c7d77b772f8c9e800268fc5c7d3c985160979f
leosupra <leosupra@protonmail.com>
2025-03-16 00:37:37 +0000

    Update game.js

--- assets/game.js
+++ assets/game.js
@@ -14,14 +14,13 @@ function preload() {
   lion = loadImage('assets/1.chd');
   rockImg = loadImage('assets/2.chd');
   bgImg = loadImage('assets/3.chd');
-  myFont = loadFont('assets/impact.ttf');
 }
 
 function setup() {
   canvas = createCanvas(windowWidth, windowHeight);
   centerCanvas();
   background(0, 0, 0);
-  textFont(myFont);
+  textFont('Impact');
   textSize(24);
   
   // Initial lion position
